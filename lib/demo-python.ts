e9d86b335c576e6b443f34adc64f145cc57216c9
Hariharan Manoharan <117761906+gdw4man@users.noreply.github.com>
2023-12-08 01:36:52 +0530

    Update demo-python.ts

--- lib/demo-python.ts
+++ lib/demo-python.ts
@@ -19,7 +19,22 @@ export class DemoPython extends Construct {
         architecture: Architecture.ARM_64,
         memorySize: 10240,
         timeout: Duration.seconds(20),
-      tracing: lambda.Tracing.PASS_THROUGH,
+        tracing: lambda.Tracing.PASS_THROUGH,
+        environment: {
+          OTEL_SERVICE_NAME: "staging-backend",
+          OTEL_LAMBDA_DISABLE_AWS_CONTEXT_PROPAGATION: "true",
+          AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-instrument",
+          OPENTELEMETRY_COLLECTOR_CONFIG_FILE: "/var/task/collector.yaml",
+          NEW_RELIC_LICENSE_KEY: "<api_key>",
+          NEW_RELIC_OPENTELEMETRY_ENDPOINT": "otlp.nr-data.net:443"
+        },
+        layers: [
+        lambda.LayerVersion.fromLayerVersionArn(
+          this,
+          'otel-layer',
+          'arn:aws:lambda:us-west-2:901920570463:layer:aws-otel-nodejs-arm64-ver-1-17-1:1'
+        ),
+      ],
       }
     );
 
